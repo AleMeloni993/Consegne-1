336b0f8cc44369e6e1f9a3392c235deeb1fcff7b
AleMeloni993 <alessandromelons@gmail.com>
2025-07-09 07:40:49 -0400

    S2 L3

---
+++ Unit 1/S 2/L3.py
@@ -0,0 +1,7 @@
+citta = input("Qual è la tua citta di origine? ")    # Chiedo all'utente la città di origine
+
+animale = input("Qual è il nome del tuo animale domestico? ")    # Chiedo all'utente il nome del proprio animale domestico
+
+nome_band = citta + " " + animale       # Combino i due input per generare il nome della band
+
+print("Il nome della tua band potrebbe essere: ", nome_band)     # Stampa il nome della band
